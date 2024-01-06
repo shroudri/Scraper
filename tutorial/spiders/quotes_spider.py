2a067c327b029ba845871a34f23f86ec0dab7a21
Rodrigo <loroxeraco@hotmail.com>
2024-01-07 00:47:18 +0100

    Initialize project

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class QuotesSpiderSpider(scrapy.Spider):
+    name = "quotes_spider"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com"]
+
+    def parse(self, response):
+        pass
