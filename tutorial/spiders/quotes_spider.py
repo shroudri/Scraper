fb237515e9729507ff2b1879d590f961226b6c1c
Rodrigo Gómez <loroxeraco@hotmail.com>
2024-01-07 15:15:02 +0100

    Generate spider

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,10 +1,24 @@
 import scrapy
 
-
 class QuotesSpiderSpider(scrapy.Spider):
     name = "quotes_spider"
     allowed_domains = ["quotes.toscrape.com"]
     start_urls = ["https://quotes.toscrape.com"]
 
     def parse(self, response):
+        quotes = response.xpath('//div[@class="quote"]')
+
+        # Extract quotes
+        for quote in quotes:
+            yield {
+                "text": quote.xpath(".//span[@class='text']/text()").get(),
+                "author": quote.xpath(".//small[@class='author']/text()").get(),
+                "tags": quote.xpath(".//div[@class='tags']/a[@class='tag']/text()").getall(),
+            }
+
+        # Navigate to next page
+        next_page_btn = response.xpath('//li[@class="next"]/a').attrib["href"]
+        if next_page_btn:
+            yield response.follow(next_page_btn, callback=self.parse)
+
         pass
